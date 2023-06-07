dcb741938ea8533eaaf8230951ede3cc1202920d
raisircl <rai.verma@gmail.com>
2023-06-07 11:22:12 +0530

    listview

--- MyWinApp/WinFormDay6Jun2023.Designer.cs
+++ MyWinApp/WinFormDay6Jun2023.Designer.cs
@@ -31,29 +31,27 @@
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(WinFormDay6Jun2023));
             this.tv1 = new System.Windows.Forms.TreeView();
-            this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
             this.imageListLarge = new System.Windows.Forms.ImageList(this.components);
+            this.imageListSmall = new System.Windows.Forms.ImageList(this.components);
+            this.lv1 = new System.Windows.Forms.ListView();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tv1
             // 
+            this.tv1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tv1.ImageIndex = 0;
             this.tv1.ImageList = this.imageListLarge;
-            this.tv1.Location = new System.Drawing.Point(31, 36);
+            this.tv1.Location = new System.Drawing.Point(0, 0);
             this.tv1.Name = "tv1";
             this.tv1.SelectedImageIndex = 0;
-            this.tv1.Size = new System.Drawing.Size(357, 545);
+            this.tv1.Size = new System.Drawing.Size(460, 620);
             this.tv1.TabIndex = 0;
-            // 
-            // imageListSmall
-            // 
-            this.imageListSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListSmall.ImageStream")));
-            this.imageListSmall.TransparentColor = System.Drawing.Color.Transparent;
-            this.imageListSmall.Images.SetKeyName(0, "hard-drive.png");
-            this.imageListSmall.Images.SetKeyName(1, "folder.png");
-            this.imageListSmall.Images.SetKeyName(2, "open-folder.png");
-            this.imageListSmall.Images.SetKeyName(3, "document.png");
-            this.imageListSmall.Images.SetKeyName(4, "gaming.png");
+            this.tv1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.tv1_AfterSelect);
             // 
             // imageListLarge
             // 
@@ -65,15 +63,58 @@
             this.imageListLarge.Images.SetKeyName(3, "document.png");
             this.imageListLarge.Images.SetKeyName(4, "gaming.png");
             // 
+            // imageListSmall
+            // 
+            this.imageListSmall.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageListSmall.ImageStream")));
+            this.imageListSmall.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageListSmall.Images.SetKeyName(0, "hard-drive.png");
+            this.imageListSmall.Images.SetKeyName(1, "folder.png");
+            this.imageListSmall.Images.SetKeyName(2, "open-folder.png");
+            this.imageListSmall.Images.SetKeyName(3, "document.png");
+            this.imageListSmall.Images.SetKeyName(4, "gaming.png");
+            // 
+            // lv1
+            // 
+            this.lv1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lv1.HideSelection = false;
+            this.lv1.LargeImageList = this.imageListLarge;
+            this.lv1.Location = new System.Drawing.Point(0, 0);
+            this.lv1.Name = "lv1";
+            this.lv1.Size = new System.Drawing.Size(1241, 620);
+            this.lv1.SmallImageList = this.imageListSmall;
+            this.lv1.TabIndex = 1;
+            this.lv1.UseCompatibleStateImageBehavior = false;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tv1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.lv1);
+            this.splitContainer1.Size = new System.Drawing.Size(1705, 620);
+            this.splitContainer1.SplitterDistance = 460;
+            this.splitContainer1.TabIndex = 2;
+            // 
             // WinFormDay6Jun2023
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1026, 620);
-            this.Controls.Add(this.tv1);
+            this.ClientSize = new System.Drawing.Size(1705, 620);
+            this.Controls.Add(this.splitContainer1);
             this.Name = "WinFormDay6Jun2023";
             this.Text = "WinFormDay6Jun2023";
             this.Load += new System.EventHandler(this.WinFormDay6Jun2023_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -83,5 +124,7 @@
         private System.Windows.Forms.TreeView tv1;
         private System.Windows.Forms.ImageList imageListSmall;
         private System.Windows.Forms.ImageList imageListLarge;
+        private System.Windows.Forms.ListView lv1;
+        private System.Windows.Forms.SplitContainer splitContainer1;
     }
 }
--- MyWinApp/WinFormDay6Jun2023.cs
+++ MyWinApp/WinFormDay6Jun2023.cs
@@ -8,6 +8,8 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
+using System.Xml.Linq;
+
 namespace MyWinApp
 {
     public partial class WinFormDay6Jun2023 : Form
@@ -31,24 +33,47 @@ namespace MyWinApp
 
             foreach(var drive in Directory.GetLogicalDrives())
             {
-              
                 TreeNode tnode = new TreeNode(drive);
                 tnode.ImageIndex = 0;
+                tnode.Tag = drive;
                 tnode.SelectedImageIndex = 0;
                 mypctnode.Nodes.Add(tnode);
-                foreach(var dir in Directory.GetDirectories(drive))
-                {
-                    
-                    DirectoryInfo di=new DirectoryInfo(dir);
-                    TreeNode tnodefolder = new TreeNode(di.Name);
-                    tnodefolder.ImageIndex = 1;
-                    tnodefolder.SelectedImageIndex = 2;
-                    tnodefolder.Tag = di.FullName;
 
-                    tnode.Nodes.Add(tnodefolder);
-                    
-                }
+                ListViewItem item = new ListViewItem(drive);
+                item.Tag = drive;
+                item.ImageIndex = 0;
+                lv1.Items.Add(item);
             }
+        }
+        void loadFolders(TreeNode tnode,string path)
+        {
+            lv1.Items.Clear();
+            foreach (var dir in Directory.GetDirectories(path))
+            {
+
+                DirectoryInfo di = new DirectoryInfo(dir);
+                TreeNode tnodefolder = new TreeNode(di.Name);
+
+                tnodefolder.ImageIndex = 1;
+                tnodefolder.SelectedImageIndex = 2;
+                tnodefolder.Tag = di.FullName;
+
+                tnode.Nodes.Add(tnodefolder);
+
+                ListViewItem item = new ListViewItem(di.Name);
+                item.Tag = di.FullName;
+                item.ImageIndex = 1;
+                lv1.Items.Add(item);
+
+            }
+        }
+
+        private void tv1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+          string path=e.Node.Tag as string;
+          if(!string.IsNullOrEmpty(path))    
+            loadFolders(e.Node,path);    
+            
         }
     }
 }
